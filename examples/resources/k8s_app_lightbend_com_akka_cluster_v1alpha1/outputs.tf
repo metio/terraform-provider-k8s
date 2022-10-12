f1cbeb4639f96a8a7f735a4cc414b96df0239216
Sebastian Hoß <seb@ho.de>
2022-10-12 16:46:12 +0200

    add example for app_lightbend_com_akka_cluster_v1alpha1

--- examples/resources/k8s_app_lightbend_com_akka_cluster_v1alpha1/outputs.tf
+++ examples/resources/k8s_app_lightbend_com_akka_cluster_v1alpha1/outputs.tf
@@ -1,5 +1,6 @@
 output "resources" {
   value = {
     "minimal" = k8s_app_lightbend_com_akka_cluster_v1alpha1.minimal.yaml
+    "example" = k8s_app_lightbend_com_akka_cluster_v1alpha1.example.yaml
   }
 }
--- examples/resources/k8s_app_lightbend_com_akka_cluster_v1alpha1/resource.tf
+++ examples/resources/k8s_app_lightbend_com_akka_cluster_v1alpha1/resource.tf
@@ -4,3 +4,59 @@ resource "k8s_app_lightbend_com_akka_cluster_v1alpha1" "minimal" {
     namespace = "some-namespace"
   }
 }
+
+resource "k8s_app_lightbend_com_akka_cluster_v1alpha1" "example" {
+  metadata = {
+    name      = "akka-cluster-demo"
+  }
+  spec = {
+    replicas = 3
+    selector = {
+      match_labels = {
+        "app.kubernetes.io/name" = "some-name"
+      }
+    }
+    template = {
+      spec = {
+        containers = [
+          {
+            name = "main"
+            image = "lightbend-docker-registry.bintray.io/lightbend/akka-cluster-demo:1.1.0"
+            readiness_probe = {
+              http_get = {
+                path = "/ready"
+                port = "management"
+              }
+              period_seconds: 10
+              failure_threshold: 10
+              initial_delay_seconds: 20
+            }
+            liveness_probe = {
+              http_get = {
+                path = "/alive"
+                port = "management"
+              }
+              period_seconds: 10
+              failure_threshold: 10
+              initial_delay_seconds: 20
+            }
+            ports = [
+              {
+                name = "http"
+                container_port = 8080
+              },
+              {
+                name = "remoting"
+                container_port = 2552
+              },
+              {
+                name = "management"
+                container_port = 8558
+              },
+            ]
+          },
+        ]
+      }
+    }
+  }
+}
