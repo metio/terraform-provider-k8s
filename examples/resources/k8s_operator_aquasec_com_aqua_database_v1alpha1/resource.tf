d74c1a7a48cd00267c9d35ef78dc2b8addbe8fa0
Sebastian Hoß <seb@ho.de>
2022-10-13 13:29:33 +0200

    extend examples for Aqua Security Operator

--- examples/resources/k8s_operator_aquasec_com_aqua_database_v1alpha1/resource.tf
+++ examples/resources/k8s_operator_aquasec_com_aqua_database_v1alpha1/resource.tf
@@ -15,3 +15,34 @@ resource "k8s_operator_aquasec_com_aqua_database_v1alpha1" "minimal" {
     }
   }
 }
+
+resource "k8s_operator_aquasec_com_aqua_database_v1alpha1" "example" {
+  metadata = {
+    name      = "test"
+    namespace = "test"
+  }
+  spec = {
+    common = {
+      active_active     = true
+      image_pull_secret = "aqua-registry"
+      split_db          = false
+    }
+    deploy = {
+      replicas = 12
+      service  = "ClusterIP"
+      image = {
+        registry    = "registry.aquasec.com"
+        repository  = "database"
+        tag         = "<<IMAGE TAG>>"
+        pull_policy = "Always"
+      }
+    }
+    disk_size       = 120
+    run_as_non_root = false
+    infra = {
+      requirements    = true
+      service_account = "aqua-sa"
+      version         = "2022.4"
+    }
+  }
+}
