cb8e5ffb8f02302656220f4cae146699d8837121
Sebastian Hoß <seb@ho.de>
2022-10-13 15:19:37 +0200

    add ACK service controller for AWS Identity & Access Management resources

---
+++ examples/resources/k8s_iam_services_k8s_aws_group_v1alpha1/resource.tf
@@ -0,0 +1,5 @@
+resource "k8s_iam_services_k8s_aws_group_v1alpha1" "minimal" {
+  metadata = {
+    name = "test"
+  }
+}
---
+++ examples/resources/k8s_iam_services_k8s_aws_policy_v1alpha1/outputs.tf
@@ -0,0 +1,5 @@
+output "resources" {
+  value = {
+    "minimal" = k8s_iam_services_k8s_aws_policy_v1alpha1.minimal.yaml
+  }
+}
