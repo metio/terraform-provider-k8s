b6994aa2ebb916b8c8d6ee78dbb3aa206793b4b1
Sebastian Hoß <seb@ho.de>
2022-10-13 13:29:33 +0200

    extend examples for Aqua Security Operator

--- examples/resources/k8s_operator_aquasec_com_aqua_kube_enforcer_v1alpha1/outputs.tf
+++ examples/resources/k8s_operator_aquasec_com_aqua_kube_enforcer_v1alpha1/outputs.tf
@@ -1,5 +1,6 @@
 output "resources" {
   value = {
     "minimal" = k8s_operator_aquasec_com_aqua_kube_enforcer_v1alpha1.minimal.yaml
+    "example" = k8s_operator_aquasec_com_aqua_kube_enforcer_v1alpha1.example.yaml
   }
 }
