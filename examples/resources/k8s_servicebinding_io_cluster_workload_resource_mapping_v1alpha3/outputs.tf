87b24cbc8f20e03767981f8090fac656c85729fa
Sebastian Hoß <seb@ho.de>
2022-10-22 18:04:08 +0200

    add service binding operator resources

---
+++ examples/resources/k8s_servicebinding_io_cluster_workload_resource_mapping_v1alpha3/outputs.tf
@@ -0,0 +1,5 @@
+output "resources" {
+  value = {
+    "minimal" = k8s_servicebinding_io_cluster_workload_resource_mapping_v1alpha3.minimal.yaml
+  }
+}
---
+++ examples/resources/k8s_servicebinding_io_cluster_workload_resource_mapping_v1alpha3/resource.tf
@@ -0,0 +1,5 @@
+resource "k8s_servicebinding_io_cluster_workload_resource_mapping_v1alpha3" "minimal" {
+  metadata = {
+    name = "test"
+  }
+}
