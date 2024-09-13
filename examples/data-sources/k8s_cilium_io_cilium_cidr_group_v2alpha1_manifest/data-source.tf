77b76ea1f5eba7430c78207b68462b1f7f6d2a39
Sebastian Hoß <seb@xn--ho-hia.de>
2024-09-13 16:33:55 +0200

    fix examples
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- examples/data-sources/k8s_cilium_io_cilium_cidr_group_v2alpha1_manifest/data-source.tf
+++ examples/data-sources/k8s_cilium_io_cilium_cidr_group_v2alpha1_manifest/data-source.tf
@@ -1,6 +1,8 @@
 data "k8s_cilium_io_cilium_cidr_group_v2alpha1_manifest" "example" {
   metadata = {
     name = "some-name"
-
+  }
+  spec = {
+    external_cidrs = []
   }
 }
--- examples/data-sources/k8s_notification_toolkit_fluxcd_io_receiver_v1beta1_manifest/data-source.tf
+++ examples/data-sources/k8s_notification_toolkit_fluxcd_io_receiver_v1beta1_manifest/data-source.tf
@@ -6,5 +6,8 @@ data "k8s_notification_toolkit_fluxcd_io_receiver_v1beta1_manifest" "example" {
   spec = {
     type      = "generic"
     resources = []
+    secret_ref = {
+      name = "some-secret"
+    }
   }
 }
